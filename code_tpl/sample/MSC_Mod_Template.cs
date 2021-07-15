b69d71d2eda9bce0e463014af248a56ed3825285
Xavier Belanche <xbelanch@protonmail.com>
2021-07-15 20:10:46 +0200

    Add simplest msc mod template

---
+++ code_tpl/sample/MSC_Mod_Template.cs
@@ -0,0 +1,22 @@
+﻿using MSCLoader;
+
+namespace MSC_Mod_Template
+{
+    public class MSC_Mod_Template : Mod
+    {
+        public override string ID => "MSC_TPL";
+        public override string Name => "Simple MSC Template";
+        public override string Author => "Kosola";
+        public override string Version => "1.0";
+        public override string Description => "Just a simple MSC Mod Template";
+
+        public override void MenuOnLoad()
+        {
+            base.MenuOnLoad();
+            if (ModLoader.CurrentScene == CurrentScene.MainMenu)
+            {
+                ModPrompt.CreatePrompt("Hello MSC World!", "That's the way");
+            }
+        }
+    }
+}
