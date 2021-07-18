1aedbbaa0e9f479303a80493503951d540b1ed8c
Xavier Belanche <xbelanch@protonmail.com>
2021-07-19 00:18:47 +0200

    Add simple open bedroom door script at the start of game

--- mods/Clock/Clock/Clock.cs
+++ mods/Clock/Clock/Clock.cs
@@ -1,6 +1,8 @@
 ﻿using MSCLoader;
 using UnityEngine;
+// using UnityEngine.UI;
 using HutongGames.PlayMaker;
+using System.IO;
 
 namespace Clock
 {
@@ -13,7 +15,29 @@ namespace Clock
 
         // Set this to true if you will be load custom assets from Assets folder.
         // This will create subfolder in Assets folder for your mod.
-        // public override bool UseAssetsFolder => false;
+        public bool UseAssetsFolder => true;
+
+
+        private GameObject clock;
+        private GameObject clockMesh;
+        private GameObject clockData;
+        
+
+        //private uint hour;
+        //private double minutes;
+
+        //private FsmFloat globalTime;
+        //private FsmFloat minutes;
+        //private FsmFloat hours;
+
+
+        private FsmBool isGameLoaded = false;
+
+
+        private Animation _doorAnimation;
+        private FsmBool _handleDoor;
+        private FsmFloat _clockMinutes;
+
 
         public override void OnNewGame()
         {
@@ -25,6 +49,22 @@ namespace Clock
         {
             // Called once, when mod is loading after game is fully loaded
             ModConsole.Log("Clock mod has loaded.");
+
+            _clockMinutes = FsmVariables.GlobalVariables.FindFsmFloat("ClockMinutes");
+            // string logHours = "Clock Hours: " + clockHours;
+            // string logMinutes = "Clock Minutes: " + clockMinutes;
+
+
+            // Bedroom door open animation at start of game
+            _doorAnimation = GameObject.Find("YARD/Building/BEDROOM1/DoorBedroom1/Pivot").GetComponent<Animation>();
+            _doorAnimation.Play("door_white_open");
+            _handleDoor = GameObject.Find("YARD/Building/BEDROOM1/DoorBedroom1/Pivot/Handle").GetComponent<PlayMakerFSM>().FsmVariables.FindFsmBool("DoorOpen");
+            // Fix door open/close state 
+            ModConsole.Log("_handleDoor: " + _handleDoor);
+            _handleDoor.Value = true;
+            ModConsole.Log("_handleDoor: " + _handleDoor);
+
+
         }
 
         public override void ModSettings()
@@ -47,6 +87,18 @@ namespace Clock
         public override void Update()
         {
             // Update is called once per frame
+            //int t = (int)_clockMinutes;
+            //if (t % 10 == 0) {
+            //    ModConsole.Log("Is it working?");
+            //    if (_isDoorOpen.Value == true)
+            //    {
+            //        _isDoorOpen = false;
+            //    } else
+            //    {
+            //        _isDoorOpen = true;
+            //    }
+            //}
+            
         }
     }
 }
